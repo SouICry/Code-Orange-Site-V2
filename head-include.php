0a2eadbb1c5521a9cc6e6002ec608286aef9014a
Guyue Zhou <souicry.email@gmail.com>
2015-10-03 21:53:32 -0700

    Rewrote blocks with flexbox and modified sidebar and link block layout styles

--- head-include.php
+++ head-include.php
@@ -2,5 +2,4 @@
 <meta name="viewport" content="initial-scale=1.0">
 
 <link rel="stylesheet" type="text/css" href="/css/normalize.css">
-<link rel="stylesheet" type="text/css" href="/css/materialize.css">
 <link rel="stylesheet" type="text/css" href="/css/style.css">
