96a592387b031c09acc99e4dba9ce15e3f6702f0
Guyue Zhou <souicry.email@gmail.com>
2015-10-05 04:12:47 -0700

    Header hide on scroll. Still not perfect but working

--- menu.php
+++ menu.php
@@ -2,8 +2,7 @@
 //Auto-generated content. Modify through UI, or change nav.json and rebuild
 ?>
 <a href="/About/About_Us">About</a>
-<a href="/Robots/All_Robots">Robots</a>
+<a href="/Robots/About_Our_Robots">Robots</a>
 <a href="/FIRST/About_FIRST"><i>FIRST</i></a>
 <a href="/Tutorials/All_Tutorials">Tutorials</a>
-<a href="/Media">Media</a>
-<a href="/Error">Error</a>
+<!--a href="/Media">Media</a-->
