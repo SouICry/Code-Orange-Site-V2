f6c84c37d46d6a4b3053cdff0b26bfd5baac97de
Guyue Zhou <souicry.email@gmail.com>
2015-10-18 16:00:31 -0700

    Cleaned up full page js files.

--- head-include.php
+++ head-include.php
@@ -4,4 +4,4 @@
 <link rel="stylesheet" type="text/css" href="/css/normalize.css">
 <link rel="stylesheet" type="text/css" href="/css/style.css">
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-<script type="text/javascript" src="/fullPagejs/jquery.fullPage.js"></script>
+<script type="text/javascript" src="/js/jquery.fullPage.min.js"></script>
