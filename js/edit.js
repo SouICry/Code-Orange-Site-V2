d73b1731b6449c248ba9b827dea7ae2b47540b61
Guyue Zhou <souicry.email@gmail.com>
2015-10-22 18:45:23 -0700

    Finished global sorting/editing toggle and cleaned up code

--- js/edit.js
+++ js/edit.js
@@ -29,7 +29,7 @@
 
  */
 
-
+//Disables all a href links
 function disableLink(event) {
     if (event.preventDefault) {
         event.preventDefault();
@@ -44,7 +44,6 @@ function disableLinks() {
         elements[i].addEventListener('click', disableLink, false);
     }
 }
-
 disableLinks();
 
 
@@ -60,22 +59,7 @@ $('.section-inner .content').addClass('editable').attr('contenteditable', 'true'
 
 
 //Left click menus
-
 //Fullpage sections
-
-function enable_rearrange_sections() {
-    $('.section-inner').contextMenu('update', edit_section_rearrange);
-    if ($('#fullpage').sortable("instance") == undefined) {
-        $('#fullpage').sortable();
-    }
-    else {
-        $('#fullpage').sortable("enable");
-    }
-}
-function disable_rearrange_sections() {
-    $('.section-inner').contextMenu('update', edit_section_stop_rearrange);
-    $('#fullpage').sortable("disable");
-}
 var edit_section = [
     {
         name: 'Change section background image',
@@ -94,63 +78,6 @@ var edit_section = [
         fun: function (data) {
 
         }
-    },
-    {
-        name: 'Rearrange sections',
-        fun: function (data) {
-            enable_rearrange_sections();
-        }
-    },
-    {
-        name: 'Exit rearrange mode',
-        disable: true,
-        fun: function (data) {
-            disable_rearrange_sections();
-        }
-    }
-];
-var edit_section_rearrange = [
-    {
-        name: 'Change section background image',
-        disable: true
-    },
-    {
-        name: 'Delete this section',
-        disable: true
-    },
-    {
-        name: 'Add new section',
-        disable: true
-    },
-    {
-        name: 'Rearrange sections',
-        disable: true
-    },
-    {
-        name: 'Exit rearrange mode',
-        disable: false
-    }
-];
-var edit_section_stop_rearrange = [
-    {
-        name: 'Change section background image',
-        disable: false
-    },
-    {
-        name: 'Delete this section',
-        disable: false
-    },
-    {
-        name: 'Add new section',
-        disable: false
-    },
-    {
-        name: 'Rearrange sections',
-        disable: false
-    },
-    {
-        name: 'Exit rearrange mode',
-        disable: true
     }
 ];
 
@@ -162,33 +89,6 @@ $('.section-inner > *').click(function (event) {
 
 
 //Sidebar (left)
-
-function enable_rearrange_sidebar() {
-    $('.sidebar .block').contextMenu('update', edit_sidebar_block_rearrange);
-
-    $('.sidebar .block img').contextMenu('update', edit_sidebar_block_img_rearrange);
-    $('.sidebar .img').contextMenu('update', edit_sidebar_img_rearrange);
-    $('.sidebar').contextMenu('update', edit_sidebar_rearrange);
-
-    if ($('.sidebar').sortable("instance") == undefined) {
-        $('.sidebar').sortable({
-            connectWith: ".sidebar"
-        });
-    }
-    else {
-        $('.sidebar').sortable("enable");
-    }
-}
-function disable_rearrange_sidebar() {
-    $('.sidebar .block').contextMenu('update', edit_sidebar_block_stop_rearrange);
-
-    $('.sidebar .block img').contextMenu('update', edit_sidebar_block_img_stop_rearrange);
-    $('.sidebar .img').contextMenu('update', edit_sidebar_img_stop_rearrange);
-    $('.sidebar').contextMenu('update', edit_sidebar_stop_rearrange);
-
-    $('.sidebar').sortable("disable");
-}
-
 var edit_sidebar = [{
     name: 'Add image to sidebar',
     fun: function (data) {
@@ -201,67 +101,12 @@ var edit_sidebar = [{
 
         }
     },
-    {
-        name: 'Rearrange sidebars (works across different sidebars)',
-        fun: enable_rearrange_sidebar
-    },
-    {
-        name: 'Exit rearrange mode',
-        disable: true,
-        fun: disable_rearrange_sidebar
-    },
     {name: '', disable: true},
     {
         name: 'Add a sidebar somewhere else',
         fun: function (data) {
 
         }
-    }
-];
-var edit_sidebar_rearrange = [
-    {
-        name: 'Add image to sidebar',
-        disable: true
-    },
-    {
-        name: 'Add block to sidebar',
-        disable: true
-    },
-    {
-        name: 'Rearrange sidebars (works across different sidebars)',
-        disable: true
-    },
-    {
-        name: 'Exit rearrange mode',
-        disable: false
-    },
-    {name: '', disable: true},
-    {
-        name: 'Add a sidebar somewhere else',
-        disable: true
-    }
-];
-var edit_sidebar_stop_rearrange = [
-    {
-        name: 'Add image to sidebar',
-        disable: false
-    },
-    {
-        name: 'Add block to sidebar',
-        disable: false
-    },
-    {
-        name: 'Rearrange sidebars (works across different sidebars)',
-        disable: false
-    },
-    {
-        name: 'Exit rearrange mode',
-        disable: true
-    },
-    {name: '', disable: true},
-    {
-        name: 'Add a sidebar somewhere else',
-        disable: false
     }
 ];
 
@@ -273,18 +118,7 @@ var edit_sidebar_img = [{
 },
     {name: '', disable: true}
 ].concat(edit_sidebar);
-var edit_sidebar_img_rearrange = [{
-    name: 'Delete this image',
-    disable: true
-},
-    {name: '', disable: true}
-].concat(edit_sidebar_rearrange);
-var edit_sidebar_img_stop_rearrange = [{
-    name: 'Delete this image',
-    disable: false
-},
-    {name: '', disable: true}
-].concat(edit_sidebar_stop_rearrange);
+
 
 var edit_sidebar_block = [{
     name: 'Delete this block',
@@ -294,18 +128,6 @@ var edit_sidebar_block = [{
 },
     {name: '', disable: true}
 ].concat(edit_sidebar);
-var edit_sidebar_block_rearrange = [{
-    name: 'Delete this block',
-    disable: true
-},
-    {name: '', disable: true}
-].concat(edit_sidebar_rearrange);
-var edit_sidebar_block_stop_rearrange = [{
-    name: 'Delete this block',
-    disable: false
-},
-    {name: '', disable: true}
-].concat(edit_sidebar_stop_rearrange);
 
 var edit_sidebar_block_img = [{
     name: 'Add image instead of text',
@@ -314,16 +136,6 @@ var edit_sidebar_block_img = [{
     }
 }
 ].concat(edit_sidebar_block);
-var edit_sidebar_block_img_rearrange = [{
-    name: 'Add image instead of text',
-    disable: true
-}
-].concat(edit_sidebar_block_rearrange);
-var edit_sidebar_block_img_stop_rearrange = [{
-    name: 'Add image instead of text',
-    disable: false
-}
-].concat(edit_sidebar_block_stop_rearrange);
 
 $('.sidebar .img').contextMenu(edit_sidebar_img);
 $('.sidebar .img').click(function (event) {
@@ -337,37 +149,10 @@ $('.sidebar .block > *').click(function (event) {
 });
 $('.sidebar .block img').contextMenu(edit_sidebar_block_img);
 
-$('.sidebar').contextMenu(edit_sidebar);
+$('.sidbar').contextMenu(edit_sidebar);
 
 
 //Blocks section
-
-function enable_rearrange_blocks_section() {
-    if ($('.blocks').sortable("instance") == undefined) {
-        $('.blocks').sortable({
-            connectWith: ".sidebar"
-        });
-    }
-    else {
-        $('.blocks').sortable("enable");
-    }
-    $('.blocks.manage').contextMenu('update', edit_blocks_section_rearrange);
-    $('.blocks.manage a.manage').contextMenu('update', edit_blocks_section_block_rearrange);
-
-    $('.blocks.manage a.manage img').contextMenu('update', edit_blocks_section_block_img_rearrange);
-
-
-}
-function disable_rearrange_blocks_section() {
-    $('.blocks').sortable("disable");
-    $('.blocks.manage').contextMenu('update', edit_blocks_section_stop_rearrange);
-    $('.blocks.manage a.manage').contextMenu('update', edit_blocks_section_block_stop_rearrange);
-
-
-    $('.blocks.manage a.manage img').contextMenu('update', edit_blocks_section_block_img_stop_rearrange);
-
-
-}
 
 var edit_blocks_section = [{
     name: 'Add block to this section',
@@ -376,51 +161,10 @@ var edit_blocks_section = [{
     }
 },
     {
-        name: 'Rearrange blocks (works across different block sections)',
-        fun: enable_rearrange_blocks_section
-    },
-    {
-        name: 'Exit rearrange mode',
-        disable: true,
-        fun: disable_rearrange_blocks_section
-    },
-    {
         name: 'Delete entire blocks section',
         fun: function (data) {
 
         }
-    }];
-var edit_blocks_section_rearrange = [{
-    name: 'Add block to this section',
-    disable: true
-},
-    {
-        name: 'Rearrange blocks (works across different block sections)',
-        disable: true
-    },
-    {
-        name: 'Exit rearrange mode',
-        disable: false
-    },
-    {
-        name: 'Delete entire blocks section',
-        disable: true
-    }];
-var edit_blocks_section_stop_rearrange = [{
-    name: 'Add block to this section',
-    disable: false
-},
-    {
-        name: 'Rearrange blocks (works across different block sections)',
-        disable: false
-    },
-    {
-        name: 'Exit rearrange mode',
-        disable: true
-    },
-    {
-        name: 'Delete entire blocks section',
-        disable: false
     }];
 
 var edit_blocks_section_block = [{
@@ -437,26 +181,6 @@ var edit_blocks_section_block = [{
     },
     {name: '', disable: true}
 ].concat(edit_blocks_section);
-var edit_blocks_section_block_rearrange = [{
-    name: 'Add image instead of text',
-    disable: true
-},
-    {
-        name: 'Delete this block',
-        disable: true
-    },
-    {name: '', disable: true}
-].concat(edit_blocks_section_rearrange);
-var edit_blocks_section_block_stop_rearrange = [{
-    name: 'Add image instead of text',
-    disable: false
-},
-    {
-        name: 'Delete this block',
-        disable: false
-    },
-    {name: '', disable: true}
-].concat(edit_blocks_section_stop_rearrange);
 
 var edit_blocks_section_block_img = [{
     name: 'Add text instead of image',
@@ -472,34 +196,9 @@ var edit_blocks_section_block_img = [{
     },
     {name: '', disable: true}
 ].concat(edit_blocks_section);
-var edit_blocks_section_block_img_rearrange = [{
-    name: 'Add text instead of image',
-    disable: true
-},
-    {
-        name: 'Delete this block',
-        disable: true
-    },
-    {name: '', disable: true}
-].concat(edit_blocks_section_rearrange);
-var edit_blocks_section_block_img_stop_rearrange = [{
-    name: 'Add text instead of image',
-    disable: false
-},
-    {
-        name: 'Delete this block',
-        disable: false
-    },
-    {name: '', disable: true}
-].concat(edit_blocks_section_stop_rearrange);
-
 
 $('.blocks.manage').contextMenu(edit_blocks_section);
 $('.blocks.manage a.manage').contextMenu(edit_blocks_section_block);
-//$('.blocks.manage a.manage > h2, .blocks.manage a.manage > .view, .blocks.manage a.manage > .type').click(function (event) {
-//    event.preventDefault();
-//    event.stopPropagation();
-//});
 $('.blocks.manage a.manage > *').click(function (event) {
     event.preventDefault();
     event.stopPropagation();
@@ -507,14 +206,147 @@ $('.blocks.manage a.manage > *').click(function (event) {
 $('.blocks.manage a.manage img').contextMenu(edit_blocks_section_block_img);
 
 
-//if ($('.blocks').sortable("instance") == undefined) {
-//    $('.blocks').sortable({
-//        connectWith: ".sidebar"
-//    });
-//}
-//else {
-//    $('.blocks').sortable("enable");
-//}
+
+
+//Sortable enable/disable
+
+function enable_rearrange_fullpage_sections() {
+    if ($('#fullpage').sortable("instance") == undefined) {
+        $('#fullpage').sortable();
+    }
+    else {
+        $('#fullpage').sortable("enable");
+    }
+}
+function disable_rearrange_fullpage_sections() {
+    $('#fullpage').sortable("disable");
+}
+
+function enable_rearrange_sidebar() {
+    if ($('.sidebar').sortable("instance") == undefined) {
+        $('.sidebar').sortable({
+            connectWith: ".sidebar"
+        });
+    }
+    else {
+        $('.sidebar').sortable("enable");
+    }
+}
+function disable_rearrange_sidebar() {
+    $('.sidebar').sortable("disable");
+}
+
+function enable_rearrange_blocks_section() {
+    if ($('.blocks').sortable("instance") == undefined) {
+        $('.blocks').sortable({
+            connectWith: ".sidebar"
+        });
+    }
+    else {
+        $('.blocks').sortable("enable");
+    }
+}
+function disable_rearrange_blocks_section() {
+    $('.blocks').sortable("disable");
+}
+
+function enable_rearrange_content_section() {
+    if ($('.content-section').sortable('instance') == undefined) {
+        $('.content-section').sortable({
+            connectWith: '.content-row .content'
+        });
+    }
+    else {
+        $('.content-section').sortable('disable');
+    }
+}
+function disable_rearrange_content_section() {
+    $('.content-section').sortable('disable');
+}
+
+function enable_rearrange_content_row() {
+    if ($('.content-row:not(:has(.sidebar))').sortable('instance') == undefined) {
+        $('.content-row:not(:has(.sidebar))').sortable({
+            connectWith: '.content-row:not(:has(.sidebar))'
+        });
+    }
+    else {
+        $('.content-row:not(:has(.sidebar))').sortable('disable');
+    }
+    if ($('.content-row:has(.sidebar)').sortable('instance') == undefined) {
+        $('.content-row:has(.sidebar)').sortable({
+            connectWith: '.content-row:has(.sidebar)'
+        });
+    }
+    else {
+        $('.content-row:has(.sidebar)').sortable('disable');
+    }
+}
+function disable_rearrange_content_row() {
+    $('.content-row:not(:has(.sidebar))').sortable('disable');
+    $('.content-row:has(.sidebar)').sortable('disable');
+}
+
+function enable_rearrange_content_row_content() {
+    $('.content-row > .content > *').addClass('sorting-content');
+    $('.content-row > .content').addClass('sorting-content');
+
+    if ($('.content-row > .content').sortable("instance") == undefined) {
+        $('.content-row > .content').sortable({
+            connectWith: '.content-row > .content'
+        });
+    }
+    else {
+        $('.content-row > .content').sortable('enable');
+    }
+}
+function disable_rearrange_content_row_content() {
+    $('.content-row > .content > *').removeClass('sorting-content');
+    $('.content-row > .content').removeClass('sorting-content');
+
+    $('.content-row > .content').sortable('disable');
+}
+
+//Global toggle to enable rearrange
+function rearrange_mode() {
+    $('.editable').attr('contenteditable', false);
+
+    enable_rearrange_fullpage_sections();
+
+    enable_rearrange_content_section();
+    enable_rearrange_content_row();
+    enable_rearrange_content_row_content();
+    enable_rearrange_blocks_section();
+    enable_rearrange_sidebar();
+}
+//Global toggle to enable content editing
+function edit_mode() {
+    $('.editable').attr('contenteditable', true);
+
+    disable_rearrange_fullpage_sections();
+
+    disable_rearrange_content_section();
+    disable_rearrange_content_row();
+    disable_rearrange_content_row_content();
+    disable_rearrange_blocks_section();
+    disable_rearrange_sidebar();
+}
+
+
+
+//Testing toggle sorting/editing
+
+var t = false;
+$('#footer').click(function () {
+    t = !t;
+    if (t) {
+        rearrange_mode();
+    }
+    else {
+        edit_mode()
+    }
+});
+//end test
 
 
 var manage_gallery_button =
@@ -612,53 +444,6 @@ var sheet = style.sheet;
 
 sheet.insertRule(".manage-button { display: inherit;}", 0);
 sheet.insertRule(".blocks.manage-button { display: flex;}", 0);
-
-
-//Testing toggle sorting/editing
-
-var t = false;
-$('#footer').click(function () {
-    t = !t;
-    if (t) {
-        $('.editable').attr('contenteditable', false);
-        $('.content-row > .content > *').addClass('sorting-content');
-        $('.content-row > .content').addClass('sorting-content');
-
-        $('.content-row > .content').sortable({
-            connectWith: '.content-row > .content'
-        });
-        $('.content-row:not(:has(.sidebar))').sortable({
-            connectWith: '.content-row:not(:has(.sidebar))'
-        });
-        $('.content-row:has(.sidebar)').sortable({
-            connectWith: '.content-row:has(.sidebar)'
-        });
-        $('.content-section').sortable({
-            connectWith: '.content-row .content'
-        });
-
-        enable_rearrange_sections();
-        enable_rearrange_blocks_section();
-        enable_rearrange_sidebar();
-    }
-    else {
-        $('.editable').attr('contenteditable', true);
-
-        $('.content-row > .content > *').removeClass('sorting-content');
-        $('.content-row > .content').removeClass('sorting-content');
-
-        $('.content-row > .content').sortable('disable');
-        $('.content-row:not(:has(.sidebar))').sortable('disable');
-        $('.content-row:has(.sidebar)').sortable('disable');
-        $('.content-section').sortable('disable');
-
-        disable_rearrange_sections();
-        disable_rearrange_blocks_section();
-        disable_rearrange_sidebar();
-    }
-
-});
-//end test
 
 
 CKEDITOR.config.contentsCss = '/css/style.css';
